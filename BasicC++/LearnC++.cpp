5599508b5208a78eec2b708a83a95899d0b9653b
Vishvam <vishusp77@gmail.com>
2025-02-02 23:49:21 +0530

    update

--- BasicC++/LearnC++.cpp
+++ BasicC++/LearnC++.cpp
@@ -4,3 +4,5 @@ int main(){
     std::cout<<"Hello HalfBloodPrin!"<<" How are you ?";
     return 0;
 }
+
+/*Now writing std every time is a bit time consuming so to reduce that we can use a thing. Check file LearnC++2.cpp*/
