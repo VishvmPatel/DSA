b348bda80073d3cd8c9d7f97e8e239204274f521
Vishvam <vishusp77@gmail.com>
2025-02-05 16:18:35 +0530

    update

--- BasicC++/LearnC++5.cpp
+++ BasicC++/LearnC++5.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 //Write a program that takes an input of age and prints if you are adult or not
-
+// age >=18 then adult , and if age < 18 then not an adult
 int main(){
-
+    int age;
+    cin >> age;
+    if(age >= 18){
+        cout<<"You are an Adult";
+    }else{
+        cout<<"You are not an Adult";
+    }
     return 0;
 }
