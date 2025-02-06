8a90c8a50450cc366a4a96d0cebfbe55e3e3a9d0
Vishvam <vishusp77@gmail.com>
2025-02-07 00:03:05 +0530

    update

---
+++ Patterns/Pattern9.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	   for(int i =0 ; i<n;i++){
+	       //space
+	        for(int j = 0 ; j<i;j++ ){
+                cout<<" ";
+            }
+            //star
+            for(int j = 0; j< 2*n -(2*i +1) ; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+	   }
+	}
+};
+
+// LOGIC BEHIND THE PATTERN 
+/*
+Input: 5
+
+Output:
+
+*********
+ *******
+  *****
+   ***
+    *
+*/
