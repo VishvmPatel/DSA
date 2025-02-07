2ef2def4507c4d3f56d24295f1326a17d97c84d1
Vishvam <vishusp77@gmail.com>
2025-02-07 18:55:58 +0530

    update

--- Patterns/Pattern13.cpp
+++ Patterns/Pattern13.cpp
@@ -4,26 +4,24 @@ using namespace std;
 class Solution {
     public:
       void printTriangle(int n) {
-        int num =1;
-        for(int i =1; i<=n ; i++){
-            for(int j = 1; j<=i;j++){
-                cout<<num<<" ";
-                num=num+1;
-                  
+        for(int i =0 ; i<n;i++){
+            for(char ch = 'A'; ch <='A'+i;ch++){
+                cout<<ch;
             }
             cout<<endl;
         }
     }
 };
 
+
 // LOGIC BEHIND THE PATTERN 
 /*
 Input: 5
 
 Output:
-1 
-2 3 
-4 5 6 
-7 8 9 10 
-11 12 13 14 15
+A
+AB
+ABC
+ABCD
+ABCDE
 */
