e94e7f79e98fc00f1e1acd4e0511acc2627cc867
Vishvam <vishusp77@gmail.com>
2025-02-17 23:36:07 +0530

    update

--- BasicC++/LearnC++26.cpp
+++ BasicC++/LearnC++26.cpp
@@ -1,15 +1,16 @@
-#include <stdio.h>
+#include <iostream>  // Use iostream instead of stdio.h
 
-//STATIC ALLOCATIONS
+// STATIC ALLOCATION
 
 int globalVar = 10;  // Statically allocated in the data segment
 
 void function() {
     int localVar = 20;  // Statically allocated in the stack
-    printf("%d\n", localVar);
+    std::cout << localVar << std::endl;  // Using C++ style output
 }
 
 int main() {
     function();
     return 0;
 }
+
