6468c3a5bb71b5377fb9b9db9fa696e8ee41b9f9
Vishvam <vishusp77@gmail.com>
2025-02-21 00:29:14 +0530

    update

---
+++ BasicC++/ListADT2.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+//USING LINKED LIST
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void remove(int value) {
+        Node* temp = head, *prev = nullptr;
+
+        if (temp != nullptr && temp->data == value) {
+            head = temp->next;
+            delete temp;
+            return;
+        }
+
+        while (temp != nullptr && temp->data != value) {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        if (temp == nullptr) return;
+
+        prev->next = temp->next;
+        delete temp;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    ~LinkedList() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+    list.display();
+    list.remove(20);
+    list.display();
+    return 0;
+}
