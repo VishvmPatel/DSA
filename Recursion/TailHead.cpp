b7270c1370663f730e04bb33be8a0a512a0b33a9
Vishvam <vishusp77@gmail.com>
2025-02-23 11:17:07 +0530

    update

---
+++ Recursion/TailHead.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void fun1(int n){
+    if(n>0){
+        cout<<n<<" ";
+        fun1(n-1);
+    }
+}
+void fun2(int n){
+    if(n>0){
+        fun2(n-1);
+        cout<<n<<" ";
+    }
+}
+
+int main(){
+    int x=3;
+    fun1(x);
+    cout<<"\n";
+    fun2(x);
+}
